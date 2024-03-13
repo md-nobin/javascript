c799876812c00a150c0c9a1b44998b7412631a76
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-13 22:01:24 +0000

    var & const

---
+++ variable constant/app.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail= "farhanahmednobin@gmail.com"
+let accountState;
+
+/* 
+    Prefer not to use ver
+    because of issue in block scope andfunctional scope
+*/
+var accountPassword = "1234"
+
+// accountID = 2 //not alowed
+
+console.table([accountID, accountEmail, accountPassword, accountState])
+console.log(accountID, accountEmail, accountPassword, accountState)
