a24e0e5555b015883b669851d99f39a3d9f85062
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-14 21:53:04 +0000

    data types

---
+++ Data Types and ECMA/app.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+// alert("Hello") //using nodejs, not browser
+
+let name = "Nobin"
+let age = 13
+let goodBoy = true
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+// object
+
+console.log(typeof goodBoy)
+console.log(typeof null) // object
