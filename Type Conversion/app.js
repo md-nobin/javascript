85f1f505c5b0b923a0987d747a90c8928987d9ac
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-15 11:00:31 +0000

    Conversion

---
+++ Type Conversion/app.js
@@ -0,0 +1,15 @@
+let score = "33a"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33asd" => NaN
+// true => 1
+// fals => 0
+// undefind => Nan 
+// null => 0
