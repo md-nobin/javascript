bcff6db78c4a31bcd7708e37feac4be1e52acb82
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-15 18:12:37 +0000

    Operations

---
+++ Operations/app.js
@@ -0,0 +1,33 @@
+let value = 9
+let nagValue = -value
+
+console.log(nagValue)
+
+console.log(5+5)
+console.log(5-4)
+console.log(5*5)
+console.log(5**5)
+console.log(5/5)
+console.log(6%3)
+
+let firstName = "MD "
+let lastName = "Nobin"
+let fullName = firstName+lastName
+
+console.log(fullName)
+
+console.log("1"+ 2)
+console.log(1 +"2")
+console.log("1"+"2"+ 3)
+console.log(1+"3"+1)
+console.log("1"+2+2)
+
+console.log(1+2+"3")
+
+let mum1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
