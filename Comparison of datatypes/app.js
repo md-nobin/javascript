50218ffc00c70233c50726f43998190988ebf613
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-15 18:29:35 +0000

    Comparison

---
+++ Comparison of datatypes/app.js
@@ -0,0 +1,17 @@
+console.log(2>1)
+console.log(1>2)
+
+console.log("2" > 1)
+console.log("1" > 2)
+console.log("2" === 2)
+
+// 👇 should note use this type of conversion 
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined>= 0)
