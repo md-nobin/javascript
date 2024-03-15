449eef5b45dff977b003708c3299b28767650cbd
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-15 11:13:35 +0000

    Conversion

--- Type Conversion/app.js
+++ Type Conversion/app.js
@@ -13,3 +13,19 @@ console.log(valueInNumber)
 // fals => 0
 // undefind => Nan 
 // null => 0
+
+let isLoggenIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggenIn)
+console.log(typeof booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "abc" => true
+
+let somNumber = 44
+
+let stringNumber = String(somNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
