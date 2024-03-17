5861d6bf457f5165007cefbbc541c88b92d35936
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-17 18:44:09 +0000

    Number methods

---
+++ Number/app.js
@@ -0,0 +1,14 @@
+let score = 200
+const balance = new Number(100)
+
+console.log(score)
+console.log(balance)
+
+console.log(balance.toString)
+console.log(balance.toFixed(2))
+
+const num = 12.978
+console.log(num.toPrecision(2))
+
+const num1 = 10000000
+console.log(num1.toLocaleString())
