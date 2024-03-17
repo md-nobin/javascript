25c43bb234ae6b0102e8e5bf4ec7c68de24aeffb
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-17 19:06:35 +0000

    Math function

---
+++ Math/app.js
@@ -0,0 +1,16 @@
+console.log(Math)
+
+console.log(Math.abs(-2))
+console.log(Math.round(4.43))
+console.log(Math.ceil(4.9))
+console.log(Math.floor(4.9))
+console.log(Math.max(9,4,6,10))
+console.log(Math.min(9,4,6,10))
+
+console.log(Math.floor(Math.random()))
+console.log((Math.random()*10) +1)
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
