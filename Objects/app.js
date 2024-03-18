54074384aca56363db9bd7bb25379d00bd6bff01
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-18 21:58:21 +0000

    object

---
+++ Objects/app.js
@@ -0,0 +1,16 @@
+// singleton
+Object.create
+
+// Object literals
+
+const user = {
+    name: "Unknown",
+    age: 10,
+    location: "Dhaka",
+    email: "unknown@gmail.com",
+    loggin: false,
+    lastLoginDay: ["Monday", "Saturday"]
+
+}
+
+console.log(user['email'])
