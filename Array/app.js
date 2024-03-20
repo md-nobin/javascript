7bdb2c4c85e67198c0c94269c9d75aef0435bdfe
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-20 19:09:46 +0000

    array in-depth

--- Array/app.js
+++ Array/app.js
@@ -1,37 +1,37 @@
 const food = ["Pizza", "Sushi", "Burger", "Salad", "Tacos", "Pasta", "Steak", "Chicken Curry", "Pancakes", "Sushi Rolls"]
 
-// console.log(food[3])
+console.log(food[3])
 
-// food.push("Meet")
-// console.log(food)
+food.push("Meet")
+console.log(food)
 
-// food.pop()
-// console.log(food)
+food.pop()
+console.log(food)
 
-// food.unshift("Bread")
-// console.log(food)
+food.unshift("Bread")
+console.log(food)
 
-// food.shift()
-// console.log(food)
+food.shift()
+console.log(food)
 
-// console.log(food.includes("Sushi"))
+console.log(food.includes("Sushi"))
 
 // Important for interview. Deference between  slice and splice.
 
 const newFood = ["Lasagna", "Burrito", "Fried Chicken", "Ice-Cream", "Miso Soup"]
 
-// let popular = newFood.slice(2, 4)
+let popular = newFood.slice(2, 4)
 
-// console.log(newFood)
-// console.log(popular)
-// console.log(newFood)
+console.log(newFood)
+console.log(popular)
+console.log(newFood)
 
-// let unLiked = newFood.splice(2, 4)
-// console.log(unLiked)
-// console.log(newFood)
+let unLiked = newFood.splice(2, 4)
+console.log(unLiked)
+console.log(newFood)
 
-// food.push(newFood)
-// console.log(food)
+food.push(newFood)
+console.log(food)
 
 allFood = food.concat(newFood)
 console.log(allFood);
