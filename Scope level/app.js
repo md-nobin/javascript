102315b9df9e6c86b7a4d166bb32ed7bff0f123f
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-20 17:01:30 +0000

    scope

---
+++ Scope level/app.js
@@ -0,0 +1,23 @@
+if (true){
+    const username = "Unknown"
+    if (username === "Unknown"){
+        const address = "Dhaka"
+        console.log(`User name: ${username}, User address: ${address}`)
+    }
+
+    // console.log(address)
+}
+
+// console.log(username)
+
+
+addone(5)
+function addone(num){
+    return num + 1
+}
+
+
+// addTwo(5)  can't be execute
+const addTwo = function(num){
+    return num + 2
+}
