5d1518bdda5e2595b3e434cfcd09d7b61ab26cab
md.nobin <105407321+md-nobin@users.noreply.github.com>
2024-03-22 18:30:59 +0000

    while loop

---
+++ While and do while loop/app.js
@@ -0,0 +1,21 @@
+index = 0
+while(index <= 10){
+    console.log(index)
+    index = index + 2 
+}
+
+const food = ["Pizza", "Sushi", "Burger", "Salad", "Tacos", "Pasta", "Steak", "Chicken Curry", "Pancakes", "Sushi Rolls"]
+let num = 0
+while (num <= food.length){
+    console.log(`Best selling food ${food[num]}`)
+    num = num + 1
+}
+
+
+score = 1
+do {
+    console.log(`score is ${score}`)
+    score++
+} while(score <= 10)
+
+
